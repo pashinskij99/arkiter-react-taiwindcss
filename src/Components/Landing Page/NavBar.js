fdaccd2fd363c5022e2b90b5542e6f96290a2802
pashinskij99 <pashinskij99@icloud.com>
2023-06-20 12:30:42 +0300

    add mobile menu

--- src/Components/Landing Page/NavBar.js
+++ src/Components/Landing Page/NavBar.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import logo from '../../Assets/logoname.png';
 import ButtonPrimary from "../UI/Button";
-import {ReactComponent as LogoIcon} from '../../Assets/icons/logo.svg'
-import {ReactComponent as LogoMobileIcon} from '../../Assets/icons/logo-mobile.svg'
+import { ReactComponent as LogoIcon } from '../../Assets/icons/logo.svg'
+import { ReactComponent as LogoMobileIcon } from '../../Assets/icons/logo-mobile.svg'
+import { ReactComponent as ArrowLeftIcon } from '../../Assets/icons/arrow-left.svg'
 import { Link } from 'react-router-dom';
+import styles from '../../styles/NavBar.module.scss'
+import clsx from 'clsx';
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -16,7 +19,7 @@ const Navbar = () => {
     const navItemsDesktopClassNames = 'text-white text-[2.8rem] hover:text-gray-300 leading-[1]'
 
     return (
-        <nav className="bg-[#0A265F] text-white py-[2.1rem] border-b-[.1rem] border-[#ffffff26] z-10 relative">
+        <nav className="bg-[#0A265F] text-white py-[2.1rem] border-b-[.1rem] border-[#ffffff26] z-50 relative">
             <div className="container flex items-center justify-between">
 
                 <Link to='/' className="flex items-center">
@@ -25,7 +28,7 @@ const Navbar = () => {
                 </Link>
 
                 {/* Mobile Menu */}
-                <div className="md:hidden">
+                <div className="md:hidden relative z-50">
                     <button
                         className="text-white w-full flex justify-center hover:text-gray-300"
                         onClick={handleMobileMenuToggle}
@@ -52,41 +55,63 @@ const Navbar = () => {
                         </svg>
                     </button>
 
-                    {isMobileMenuOpen && (
-                        <div className="bg-[#0A265F] py-4 text-center">
-                            <a
-                                href="#"
-                                className="block py-4 text-[4.8rem] text-white hover:text-gray-300"
-                            >
-                                Home
-                            </a>
-                            <a
-                                href="#"
-                                className="block py-4 text-[4.8rem] text-white hover:text-gray-300"
-                            >
-                                About
-                            </a>
-                            <a
-                                href="#"
-                                className="block py-4 text-[4.8rem] text-white hover:text-gray-300"
-                            >
-                                Why us?
-                            </a>
-                            <a
-                                href="#"
-                                className="block py-4 text-[4.8rem] text-white hover:text-gray-300"
-                            >
-                                Pricing
-                            </a>
-                            <a
-                                href="#"
-                                className="block py-4 text-[4.8rem] text-white hover:text-gray-300"
-                            >
-                                Blog
-                            </a>
+                    {/* {isMobileMenuOpen && ( */}
+                    <div className={clsx("bg-[#0A265F] py-4 text-center flex flex-col justify-between fixed top-0 right-0 z-50 h-full max-w-[300px]", styles.mobileMenu, {
+                        ['!translate-x-0']: isMobileMenuOpen
+                    })}>
+                        <div>
+                            <div onClick={handleMobileMenuToggle} className='flex items-center p-[32px] cursor-pointer'>
+                                <ArrowLeftIcon className={styles.arrowIcon} />
+                                <span className='font-medium leading-[1] text-[4rem] ml-[20px]'>Continue Exploring</span>
+                            </div>
+
+                            <div className='flex flex-col items-start px-[32px] pb-[24px]'>
+                                <a
+                                    href="#"
+                                    className="block py-4 text-[5.8rem] text-white hover:text-gray-300"
+                                >
+                                    Home
+                                </a>
+                                <a
+                                    href="#"
+                                    className="block py-4 text-[5.8rem] text-white hover:text-gray-300"
+                                >
+                                    About
+                                </a>
+                                <a
+                                    href="#"
+                                    className="block py-4 text-[5.8rem] text-white hover:text-gray-300"
+                                >
+                                    Why us?
+                                </a>
+                                <a
+                                    href="#"
+                                    className="block py-4 text-[5.8rem] text-white hover:text-gray-300"
+                                >
+                                    Pricing
+                                </a>
+                                <a
+                                    href="#"
+                                    className="block py-4 text-[5.8rem] text-white hover:text-gray-300"
+                                >
+                                    Blog
+                                </a>
+                            </div>
                         </div>
-                    )}
+
+
+                        <div className='p-[32px]'>
+                            <LogoIcon className='w-full' />
+                        </div>
+
+                    </div>
+                    {/* )} */}
                 </div>
+
+                {/* Mobile back */}
+                <div onClick={handleMobileMenuToggle} className={clsx(styles.mobileBack, {
+                    ['!opacity-100 !pointer-events-auto !touch-auto']: isMobileMenuOpen
+                })} />
 
                 {/* Desktop Menu */}
                 <div className="hidden md:flex items-center space-x-14 lg:space-x-28 text-arkiterWhite-100 translate-y-[-7%]">
