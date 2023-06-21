eed3c2e1c48638f1249231cf6fa97bf77dad9079
pashinskij99 <pashinskij99@icloud.com>
2023-06-21 18:26:51 +0300

    add click func on steps

--- src/Components/Landing Page/StageoneNew.js
+++ src/Components/Landing Page/StageoneNew.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image1 from '../../Assets/slideImageCarAnimateSection1.jpg';
 import Image2 from '../../Assets/slideImageCarAnimateSection2.jpg';
 import Image3 from '../../Assets/slideImageCarAnimateSection3.jpg';
@@ -10,19 +10,23 @@ const classNameP = 'text-white text-[2rem] leading-[2.5rem]'
 const StageoneNew = () => {
   const [active, setActive] = useState(0)
   const [procent, setProcent] = useState(0)
+  const [isInterval, setIsInterval] = useState(true)
+  const interval = useRef(null);
 
   useEffect(() => {
-    setInterval(() => {
+    interval.current = setInterval(() => {
       setProcent(prevState => {
         if(prevState === 100) return 0
         return prevState += 1
       })
     }, 100)
 
+    return () => {
+      clearInterval(interval.current)
+    }
   }, [])
 
   useEffect(() => {
-    console.log(procent === 100);
     if(procent === 100) {
       setActive(prevState => {
         const nextState = prevState + 1
@@ -53,30 +57,39 @@ const StageoneNew = () => {
 
 const stepContent = [
   {
-    id: 1,
+    id: 0,
     contentH2: 'Build your test',
     contentP: 'Unleash the power of our comprehensive architecture and ai coding assessments.',
     last: false,
-    width: active === 0 ? procent + '%' : '100%'
+    width: isInterval
+      ? (active === 0 ? procent + '%' : '100%')
+      : active === 0 ? '100%' : '0%'
   },
   {
-    id: 2,
+    id: 1,
     contentH2: 'Candidate interviews',
     contentP: 'Here your candidates undertake your tailored interview on our fully automated and engaging platform',
     last: false,
-    width: active === 1 ? procent + '%' : active < 1 ? '0%' : '100%'
+    width: isInterval
+      ? (active === 1 ? procent + '%' : active < 1 ? '0%' : '100%')
+      : active === 1 ? '100%' : '0%'
   },
   {
-    id: 3,
+    id: 2,
     contentH2: 'Hire the best',
     contentP: `Build your dream team by hiring the best candidates with our unbiased, data-driven insights.`,
     last: true,
-    width: active === 2 ? procent + '%' : '0%'
+    width: isInterval
+      ? (active === 2 ? procent + '%' : '0%')
+      : active === 2 ? '100%' : '0%'
   },
 ]
 
-
-  console.log(procent);
+  const handleClick = (index) => {
+    setActive(index)
+    clearInterval(interval.current)
+    setIsInterval(false)
+  }
 
   return (
     <div className="bg-[#0A265F] pt-[14rem] relative pb-[12.5rem]">
@@ -92,21 +105,25 @@ const stepContent = [
           <div className='grid grid-cols-1 md:grid-cols-3 mt-[10rem] gap-[7.3rem] md:gap-[17.3rem] items-start'>
           {
               stepContent.map(({ id, contentH2, contentP, last, width }, index) => (
-                <div className='flex flex-col h-full'>
+                <div className='flex flex-col h-full' onClick={() => handleClick(index)}>
                   <div className='w-full h-[4px] relative'>
 
                     <div className='w-full h-full absolute top-0 left-0' style={{
                       background: '#6C6C6C9E'
                     }} />
                     <div className='h-full absolute top-0 left-0 z-10' style={{
-                      background: 'linear-gradient(90deg, #49CCF9 100.99%, #475167 99.15%)',
+                      background: isInterval
+                        ? 'linear-gradient(90deg, #49CCF9 100.99%, #475167 99.15%)'
+                        : 'linear-gradient(90deg, #13E5C0 100.99%, #13E5C0 99.15%)' ,
                       width,
                       transition: 'all 0.3s'
                     }} />
                     <div className='h-full absolute top-0 left-0' style={{
                       filter: 'blur(4px)',
                       width,
-                      background: 'linear-gradient(90deg, #49CCF9 100.99%, #475167 99.15%)',
+                      background: isInterval
+                        ? 'linear-gradient(90deg, #49CCF9 100.99%, #475167 99.15%)'
+                        : 'linear-gradient(90deg, #13E5C0 100.99%, #13E5C0 99.15%)',
                       transition: 'all 0.3s'
                     }} />
                   </div>
